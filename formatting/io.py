6ea147656f9e3ddab8147d64fcbdc073e7343c0d
adrianpearl <adrianspearl@gmail.com>
2017-12-13 19:28:08 -0500

    integrate html map

--- formatting/io.py
+++ formatting/io.py
@@ -1,11 +1,64 @@
-rd = open("states.txt", "r")
-wr = open("output.txt", "w")
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
 
-for line in rd:
-	part_we_want = line.split("\"")[1]
-	words = " ".join(part_we_want.split("_"))
-	wr.write("<option value=\"")
-	wr.write(words)
-	wr.write("\">")
-	wr.write(words)
-	wr.write("</option> \n")
+# Read in the file
+with open('states.txt', 'r') as file :
+	filedata = file.read()
+
+# Replace the target string
+for st, ab in us_state_abbrev.items():
+	filedata = filedata.replace(st, ab)
+
+# Write the file out again
+with open('states.txt', 'w') as file:
+	file.write(filedata)

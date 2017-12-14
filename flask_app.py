5666617ec7213f65277a99143343efc72376786f
morris pearl <morris@morris.fios-router.home>
2017-12-13 20:51:05 -0500

    change whole_state queries to not use zips table

--- flask_app.py
+++ flask_app.py
@@ -120,19 +120,30 @@ state_cd_count['Wisconsin'] = 8
 state_cd_count['West Virginia'] = 3
 state_cd_count['Wyoming'] = 1
 
+def get_zipcode_list(self, state, district):
+        q = """
+            select zip
+            from zips
+            where state = ?
+            and cd = ?
+            order by zip
+            """
+        results = self.cursor.execute(q,(state,district))
+        return results
+
 def get_summary_data(state, district, whole_state):
 	print(state, district, whole_state)
 	if (whole_state):
 		q = """
-		select description as income_bracket,
-		sum(total_count),sum(single_count),sum(joint_count),
-		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
-		from agi_groups a, tax_info b, zips c
-		where a.category = b.agi_category
-		and b.zip = c.zip
-		and c.state = ?
-		group by agi_category
-		order by category"""
+                  select description as income_bracket,
+                  sum(total_count),sum(single_count),sum(joint_count),
+                  sum(head_household_count),sum(total_dependents), sum(agi) * 1000
+                  from agi_groups a, tax_info b
+                  where a.category = b.agi_category
+                  and b.zip != "99999"
+                  and b.state = ?
+                  group by agi_category
+                  order by category"""
 		results = cursor.execute(q,(state,))
 	else:
 		q = """
@@ -155,15 +166,16 @@ def get_field_data(irs_col, state, district, whole_state):
 	print(irs_col, state, district, whole_state)
 	if (whole_state):
 		q = string.Template("""
-		select description as income_bracket,
-		sum($COUNT_FIELD) as tax_return_count,
-		sum($FIELD) * 1000 as tax_return_dollars
-		from agi_groups a, tax_info b, zips c
-		where a.category = b.agi_category
-		and b.zip = c.zip
-		and c.state = ?
-		group by agi_category
-		order by category""")
+                  select description as income_bracket,
+                  sum($COUNT_FIELD) as tax_return_count,
+                  sum($FIELD) * 1000 as tax_return_dollars
+                  from agi_groups a, tax_info b
+                  where a.category = b.agi_category
+                  and b.zip != "99999"
+                  and b.state = ?
+                  group by agi_category
+                  order by category""")
+
 		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col),(state,))
 	else:
 		q = string.Template("""

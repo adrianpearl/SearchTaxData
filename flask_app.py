0782017fad05d6bd27dd633a3357769ebab17288
adrianpearl <adrianspearl@gmail.com>
2017-12-16 14:29:23 -0500

    added whole nation query, placed sqlite queries in separate file

--- flask_app.py
+++ flask_app.py
@@ -1,83 +1,7 @@
 from flask import Flask, render_template, json, request, jsonify
-import sqlite3
-import string
+import queries
 
-conn = sqlite3.connect('cd_by_zip.sqlite3')
-cursor = conn.cursor()
 one_district = ['AK', 'DE', 'DC', 'MT', 'ND', 'SD', 'VT', 'WY']
-
-def get_zipcode_list(self, state, district):
-        q = """
-            select zip
-            from zips
-            where state = ?
-            and cd = ?
-            order by zip
-            """
-        results = self.cursor.execute(q,(state,district))
-        return results
-
-def get_summary_data(state, district, whole_state):
-	print(state, district, whole_state)
-	if (whole_state):
-		q = """
-		select description as income_bracket,
-		sum(total_count),sum(single_count),sum(joint_count),
-		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
-		from agi_groups a, tax_info b
-		where a.category = b.agi_category
-		and b.zip != "99999"
-		and b.state = ?
-		group by agi_category
-		order by category"""
-		results = cursor.execute(q,(state,))
-	else:
-		q = """
-		select description as income_bracket,
-		sum(total_count),sum(single_count),sum(joint_count),
-		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
-		from agi_groups a, tax_info b, zips c
-		where a.category = b.agi_category
-		and b.zip = c.zip
-		and c.state = ?
-		and b.state = c.state
-		and c.cd = ?
-		group by agi_category
-		order by category"""
-		results = cursor.execute(q,(state,district))
-
-	return results
-
-def get_field_data(irs_col, state, district, whole_state):
-	print(irs_col, state, district, whole_state)
-	if (whole_state):
-		q = string.Template("""
-		select description as income_bracket,
-		sum($COUNT_FIELD) as tax_return_count,
-		sum($FIELD) * 1000 as tax_return_dollars
-		from agi_groups a, tax_info b
-		where a.category = b.agi_category
-		and b.zip != "99999"
-		and b.state = ?
-		group by agi_category
-		order by category""")
-    
-		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col),(state,))
-	else:
-		q = string.Template("""
-		select description as income_bracket,
-		sum($COUNT_FIELD) as tax_return_count,
-		sum($FIELD) * 1000 as tax_return_dollars
-		from agi_groups a, tax_info b, zips c
-		where a.category = b.agi_category
-		and b.zip = c.zip
-		and c.state = ?
-		and b.state = c.state
-		and c.cd = ?
-		group by agi_category
-		order by category""")
-		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col),(state,district))
-	return theresults
 
 app = Flask(__name__)
 
@@ -95,36 +19,29 @@ def signUp():
 	print(request.form)
 
 	# read the posted values from the UI
-	_taxreturnline = request.form['returnline']
-	_state = request.form['state']
-	_district = request.form['district']
-	print(_state)
-	if ('wholestate' in request.form.keys()) or (_state in one_district):
-		_wholestate = True
-	else:
-		_wholestate = False
-	print("wholestate: ", _wholestate)
+	_taxreturnline = request.form.get('returnline', '')
+	_state = request.form.get('state', '')
+	_district = request.form.get('district', '')
+	_cdstatenation = request.form['cdstatenation']
+	
+	print(_taxreturnline, _state, _district, _cdstatenation)
 	# validate the received values
-	if _taxreturnline and _state and (_district or _wholestate):
-		_taxreturnline = "_".join(_taxreturnline.split(" "))
-		if _wholestate:
-			output = get_summary_data(_state, _district, True)
-			summary = [ix for ix in output]
-			output = get_field_data(_taxreturnline, _state, _district, True)
-			taxdata = [ix for ix in output]
-		else:
+	if (not _taxreturnline) or ( (not _state) and _cdstatenation != "nation") or ( (not _district) and _cdstatenation == "cdonly"):
+		return json.dumps({'html':'<span>Invalid query</span>'})
+	else:
+		
+		if _cdstatenation == "cdonly":
 			_district = int(_district)
-			output = get_summary_data(_state, _district, False)
-			summary = [ix for ix in output]
-			output = get_field_data(_taxreturnline, _state, _district, False)
-			taxdata = [ix for ix in output]
+		_taxreturnline = "_".join(_taxreturnline.split(" "))
+		output = queries.get_summary_data(_state, _district, _cdstatenation)
+		summary = [ix for ix in output]
+		output = queries.get_field_data(_taxreturnline, _state, _district, _cdstatenation)
+		taxdata = [ix for ix in output]
 		
 		print(summary)
 		print(taxdata)
 
 		return jsonify(r_summary=summary, r_taxdata=taxdata)
-	else:
-		return json.dumps({'html':'<span>Invalid query</span>'})
 
 if __name__ == "__main__":
     app.run()
---
+++ queries.py
@@ -0,0 +1,96 @@
+import sqlite3
+import string
+
+conn = sqlite3.connect('cd_by_zip.sqlite3')
+cursor = conn.cursor()
+
+def get_zipcode_list(self, state, district):
+        q = """
+            select zip
+            from zips
+            where state = ?
+            and cd = ?
+            order by zip
+            """
+        results = self.cursor.execute(q,(state,district))
+        return results
+
+def get_summary_data(state, district, cd_state_nation):
+	print("get summary: ", state, district, cd_state_nation)
+	if (cd_state_nation == "nation"):
+		q = """
+		select description as income_bracket,
+		sum(total_count),sum(single_count),sum(joint_count),
+		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
+		from agi_groups a, tax_info b
+		where a.category = b.agi_category
+		group by agi_category
+		order by category"""
+		results = cursor.execute(q)
+	elif (cd_state_nation == "stateonly"):
+		q = """
+		select description as income_bracket,
+		sum(total_count),sum(single_count),sum(joint_count),
+		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
+		from agi_groups a, tax_info b
+		where a.category = b.agi_category
+		and b.zip != "99999"
+		and b.state = ?
+		group by agi_category
+		order by category"""
+		results = cursor.execute(q,(state,))
+	else:
+		q = """
+		select description as income_bracket,
+		sum(total_count),sum(single_count),sum(joint_count),
+		sum(head_household_count),sum(total_dependents), sum(agi) * 1000
+		from agi_groups a, tax_info b, zips c
+		where a.category = b.agi_category
+		and b.zip = c.zip
+		and c.state = ?
+		and b.state = c.state
+		and c.cd = ?
+		group by agi_category
+		order by category"""
+		results = cursor.execute(q,(state,district))
+	return results
+
+def get_field_data(irs_col, state, district, cd_state_nation):
+	print("get field data: ", irs_col, state, district, cd_state_nation)
+	if (cd_state_nation == "nation"):
+		q = string.Template("""
+		select description as income_bracket,
+		sum($COUNT_FIELD) as tax_return_count,
+		sum($FIELD) * 1000 as tax_return_dollars
+		from agi_groups a, tax_info b
+		where a.category = b.agi_category
+		group by agi_category
+		order by category""")
+		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col))
+	elif (cd_state_nation == "stateonly"):
+		q = string.Template("""
+		select description as income_bracket,
+		sum($COUNT_FIELD) as tax_return_count,
+		sum($FIELD) * 1000 as tax_return_dollars
+		from agi_groups a, tax_info b
+		where a.category = b.agi_category
+		and b.zip != "99999"
+		and b.state = ?
+		group by agi_category
+		order by category""")
+		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col),(state,))
+	else:
+		q = string.Template("""
+		select description as income_bracket,
+		sum($COUNT_FIELD) as tax_return_count,
+		sum($FIELD) * 1000 as tax_return_dollars
+		from agi_groups a, tax_info b, zips c
+		where a.category = b.agi_category
+		and b.zip = c.zip
+		and c.state = ?
+		and b.state = c.state
+		and c.cd = ?
+		group by agi_category
+		order by category""")
+		theresults = cursor.execute(q.substitute(COUNT_FIELD=irs_col+"_count",FIELD=irs_col),(state,district))
+	return theresults
